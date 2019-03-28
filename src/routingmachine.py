3372a66086cc1398b37e57eed067619c7a09321c
dsh011 <48263290+dsh011@users.noreply.github.com>
2019-03-28 16:25:11 -0500

    Add files via upload

--- src/routingmachine.py
+++ src/routingmachine.py
@@ -6,50 +6,71 @@ from branca.element import CssLink, Element, Figure, JavascriptLink, MacroElemen
 
 from jinja2 import Template
 
-
+#Note the Routing Services are using OSRMv1 demo server. This is not meant to be used for production purposes
+# It is used for this program as a demo and not a live version, please see https://github.com/Project-OSRM/osrm-backend/wiki/Api-usage-policy for more details
 class RoutingMachine(MacroElement):
-    """
-    Vector drawing and editing plugin for Leaflet.
-
-    Parameters
-    ----------
-    export : bool, default False
-        Add a small button that exports the drawn shapes as a geojson file.
-
-    Examples
-    --------
-    >>> m = folium.Map()
-    >>> Draw(export=True).add_to(m)
-
-    For more info please check
-    https://leaflet.github.io/Leaflet.draw/docs/leaflet-draw-latest.html
-
-    """
+   
     _template = Template("""
-        {% macro script(this,kwargs) %}
-            var {{this.get_name()}} = {
-                base_layers : { {% for key,val in this.base_layers.items() %}"{{key}}" : {{val}},{% endfor %} },
-                overlays : { {% for key,val in this.overlays.items() %}"{{key}}" : {{val}},{% endfor %} }
-                };
-            L.Routing.control(
-                var {{this.get_name()}} = new L.Routng.control({})
-        {% endmacro %}
+            {% macro script(this, kwargs) %}
+
+            function button(label, container) {
+                var btn = L.DomUtil.create('button', '', container);
+                btn.setAttribute('type', 'button');
+                btn.innerHTML = label;
+                return btn;
+            }
+               var control= L.Routing.control({
+                    waypoints: [],
+                    routeWhileDragging: true,
+                    position: 'bottomright',
+                    geocoder: new L.Control.Geocoder.Nominatim(),
+                    addWaypoints: true,
+                    reverseWaypoints: true,
+
+                }).addTo({{this._parent.get_name()}});
+
+
+
+                {{this._parent.get_name()}}.on('click', function (e) {
+                var container = L.DomUtil.create('div'),
+                    startBtn = button('Start from this location', container),
+                    destBtn = button('Go to this location', container);
+
+                L.DomEvent.on(startBtn, 'click', function () {
+                    control.spliceWaypoints(0, 1, e.latlng);
+                    {{this._parent.get_name()}}.closePopup();
+                });
+
+                L.DomEvent.on(destBtn, 'click', function () {
+                    control.spliceWaypoints(control.getWaypoints().length - 1, 1, e.latlng);
+                    {{this._parent.get_name()}}.closePopup();
+                });
+
+                L.popup().setContent(container).setLatLng(e.latlng).openOn({{this._parent.get_name()}});
+            });
+
+                
+
+                L.Routing.errorControl(control).addTo({{this._parent.get_name()}});
+
+            {% endmacro %}
         """)  # noqa
 
-    def __init__(self, export=False):
+    def __init__(self):
         super(RoutingMachine, self).__init__()
-        self._name = 'RouteControl'
-        self.export = export
 
     def render(self, **kwargs):
-        super(RouteControl, self).render()
+        super(RoutingMachine, self).render()
 
         figure = self.get_root()
         assert isinstance(figure, Figure), ('You cannot render this Element '
                                             'if it is not in a Figure.')
 
         figure.header.add_child(
-            JavascriptLink('https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.js'))  # noqa
+            JavascriptLink('https://cdn.jsdelivr.net/gh/dsh011/WxPathfinder/leaflet-routing-machine-3.2.12/leaflet-routing-machine.js'))  # noqa
 
         figure.header.add_child(
-            CssLink('https://unpkg.com/leaflet-routing-machine@latest/dist/leaflet-routing-machine.css'))  # noqa
+            JavascriptLink('https://unpkg.com/leaflet-control-geocoder/dist/Control.Geocoder.js'))
+
+        figure.header.add_child(
+            CssLink('https://cdn.jsdelivr.net/gh/dsh011/WxPathfinder/leaflet-routing-machine-3.2.12/leaflet-routing-machine.css'))  # noqa
